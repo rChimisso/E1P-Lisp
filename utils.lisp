e99f810f0a900ff477c387cc96e1ed9ad25ba659
Riccardo Chimisso <riccardo.chimisso@gmail.com>
2021-12-28 16:08:33 +0100

    Added support for octets.

--- utils.lisp
+++ utils.lisp
@@ -3,10 +3,11 @@
 	(:export
 		:allowed-p
 		:query-p
-		:identifier-p
-		:host-identifier-p
+		:ident-p
+		:host-ident-p
 		:alpha-p
 		:alnum-p
+		:evaluate
 	)
 )
 (in-package utils)
@@ -15,15 +16,36 @@
 	(and
 		(> code 32)
 		(< code 127)
-		(/= code 34 37 60 62 92 94 96 123 124 125)
+		(/= code 92)
 	)
 )
 (defun query-p (char) (query-code-p (char-code char)))
 (defun query-code-p (code) (and (allowed-code-p code) (/= code 35)))
-(defun identifier-p (char) (identifier-code-p (char-code char)))
-(defun identifier-code-p (code) (and (query-code-p code) (/= code 47 58 63 64)))
-(defun host-identifier-p (char) (host-identifier-code-p (char-code char)))
-(defun host-identifier-code-p (code) (and (identifier-code-p code) (/= code 46)))
+(defun ident-p (char) (ident-code-p (char-code char)))
+(defun ident-code-p (code) (and (query-code-p code) (/= code 47 58 63 64)))
+(defun host-ident-p (char) (host-ident-p-code-p (char-code char)))
+(defun host-ident-p-code-p (code) (and (ident-code-p code) (/= code 46)))
 (defun alpha-p (char) (and (alpha-char-p char) (allowed-p char)))
 (defun alnum-p (char) (and (alphanumericp char) (allowed-p char)))
-; Add octets.
+(defun octet (char)
+	(cond
+		((char= char #\Space) '(#\% #\2 #\0))
+		((char= char #\") '(#\% #\2 #\2))
+		((char= char #\%) '(#\% #\2 #\5))
+		((char= char #\<) '(#\% #\3 #\C))
+		((char= char #\>) '(#\% #\3 #\E))
+		((char= char #\^) '(#\% #\5 #\E))
+		((char= char #\`) '(#\% #\6 #\0))
+		((char= char #\{) '(#\% #\7 #\B))
+		((char= char #\|) '(#\% #\7 #\C))
+		((char= char #\}) '(#\% #\7 #\D))
+		(t (list char))
+	)
+)
+(defun convert (chars)
+	(if (cdr chars)
+		(append (octet (car chars)) (convert (cdr chars)))
+		(octet (car chars))
+	)
+)
+(defun evaluate (chars) (coerce (convert chars) 'string))
